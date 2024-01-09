b81838e9bef904e75a00ec861fcc72e74c50a86b
Gilson96 <grafael99@gmail.com>
2024-01-09 23:16:49 +0000

    Projects setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import { Fragament } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen"
+import BasketScreen from "./screens/BasketScreen"
+import DeliveryScreen from "./screens/DeliveryScreen"
+import PreparingOrderScreen from "./screens/PreperingOrderScreen"
+import RestaurantScreen from "./screens/RestaurantScreen"
 
-function App() {
+const App = ({ id }) => {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    
+      <Routes>
+        <Route path='/' element={<HomeScreen id={id} />} />
+        <Route path='/restaurant/*' element={<RestaurantScreen id={id} />} />
+        <Route path='/basketScreen' element={<BasketScreen/>}/>
+        <Route path='/preparingOrderScreen' element={<PreparingOrderScreen/>}/>
+        <Route path='/deliveryScreen' element={<DeliveryScreen/>}/>
+      </Routes>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
+import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+import App from './App'
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { Provider } from 'react-redux';
+import store from './store'
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <HashRouter >
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HashRouter >
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
 
